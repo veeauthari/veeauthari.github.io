8318b8f87a9e95309e53799cc34216d7e901b6c8
veeauthari <autha955@newschool.edu>
2020-04-10 15:01:18 -0400

    gooey

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+
 function changeColorBg(color){
 	document.bgColor = color;
 	}
+
+console.clear();
+
+const TAIL_LENGTH = 40;
+
+const cursor = document.getElementById('cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorCircles;
+let cursorHistory = Array(TAIL_LENGTH).fill({x: 0, y: 0});
+
+function onMouseMove(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}
+
+function onClick(event) {
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    cursorHistory[i].x += Math.random() * 100 - 50;
+    cursorHistory[i].y += Math.random() * 100 - 50;
+    }
+}
+
+
+function initCursor() {
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    let div = document.createElement('div');
+    div.classList.add('cursor-circle') ;
+    cursor.append(div);
+  }
+  cursorCircles = Array.from(document.querySelectorAll('.cursor-circle'));
+}
+
+function updateCursor() {  
+  cursorHistory.shift();
+  cursorHistory.push({ x: mouseX, y: mouseY });
+    
+  for (let i = 0; i < TAIL_LENGTH; i++) {
+    let current = cursorHistory[i];
+    let next = cursorHistory[i + 1] || cursorHistory[TAIL_LENGTH - 1];
+    
+    let xDiff = next.x - current.x;
+    let yDiff = next.y - current.y;
+    
+    current.x += xDiff * 0.35;
+    current.y += yDiff * 0.35;
+    cursorCircles[i].style.transform = `translate(${current.x}px, ${current.y}px) scale(${i/TAIL_LENGTH})`;  
+  }
+  requestAnimationFrame(updateCursor)
+}
+
+document.addEventListener('mousemove', onMouseMove, false);
+document.addEventListener('click', onClick, false);
+
+initCursor();
+updateCursor();
