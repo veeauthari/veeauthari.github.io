81a86d4ac62412e85388218b2ee79dd73d2dc35b
veeauthari <autha955@newschool.edu>
2020-04-20 14:24:30 -0400

    update diary

---
+++ projects/isolation/script.js
@@ -0,0 +1,20 @@
+var i = 0,images = ["assets/black-2.jpg",
+                        "assets/white-2.jpg",
+                        "assets/red-mockup-2.JPG",
+                        "assets/sun-mockup-2.jpg",
+                        "assets/pink-mockup-2.jpg"];
+                            
+          function mySlide(param)
+          {
+              if(param === 'next')
+              {
+                  i++;
+                  if(i === images.length){ i = images.length - 1; }
+              }else{
+                  i--;
+                  if(i < 0){ i = 0; }
+              }
+              
+              document.getElementById('slide').src = images[i];
+          }
+          
