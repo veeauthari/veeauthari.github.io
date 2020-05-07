b2de56319af214138db051301f87eaf240f74992
veeauthari <autha955@newschool.edu>
2020-05-07 14:56:45 -0400

    progress final

---
+++ projects/leave-your-mark/script.js
@@ -0,0 +1,23 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+$(document).ready(function() {
+
+  $(function() {
+
+    $( ".mydiv" ).draggable({
+      
+    });
+
+    });
+    // all custom jQuery will go here
+});
+
+
+// 
+
+
+// var timeout = setInterval('writeText(text)', speed);
